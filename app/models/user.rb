2404e3cfab27881a067f15ee9b8596736624b190
Kyle Samuel <Ksamuel92@gmail.com>
2021-05-25 10:42:29 -0400

    commit tree c645549e74e66913521cec76e3b149e7ea52db74

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,19 @@
 class User < ActiveRecord::Base
+    has_secure_password
+  
+    def check_balance
+        "You have #{self.balance} available for your account"
+    end
+
+    def withdraw_money(amount)
+       self.balance -= amount unless self.balance < amount
+       if nil
+        "You have no money available"
+       end
+    end
+
+    def deposit_money(amount)
+        self.balance += amount
+    end
+binding.pry
 end
